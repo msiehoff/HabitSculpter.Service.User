850943fb1d7f7d6898032f1017b43c7ac2c19330
msiehoff <matthew.siehoff@gmail.com>
2014-11-11 20:25:48 -0600

    initial commit

---
+++ HabitSculptor.Service.User/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HabitSculptor.Service.User.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ HabitSculptor.Service.User/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HabitSculptor.Service.User.WebApiApplication" Language="C#" %>
