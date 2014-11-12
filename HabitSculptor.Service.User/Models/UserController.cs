d285e5e3b99668e850e298115798a2e984f94508
msiehoff <matthew.siehoff@gmail.com>
2014-11-11 21:00:09 -0600

    user controller scaffolded

---
+++ HabitSculptor.Service.User/Models/UserController.cs
@@ -0,0 +1,112 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace HabitSculptor.Service.Users.Models
+{
+    public class UserController : ApiController
+    {
+        private UserContext db = new UserContext();
+
+        // GET api/User
+        public IQueryable<User> GetUsers()
+        {
+            return db.Users;
+        }
+
+        // GET api/User/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(long id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT api/User/5
+        public IHttpActionResult PutUser(long id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/User
+        [ResponseType(typeof(User))]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.UserId }, user);
+        }
+
+        // DELETE api/User/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(long id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserExists(long id)
+        {
+            return db.Users.Count(e => e.UserId == id) > 0;
+        }
+    }
+}
