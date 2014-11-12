7de7db948d13fadbd081ed6b2d09451ae691b63f
msiehoff <matthew.siehoff@gmail.com>
2014-11-11 20:58:21 -0600

    user model added

---
+++ HabitSculptor.Service.User/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HabitSculptor.Service.Users.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
